02e7509e0206fc0569df98a491b639b307accdcc
bunoviske <brunofcarvalho1996@gmail.com>
2020-05-29 00:29:28 +0200

    remove js folder from git

--- js/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const webHookServer_1 = require("./webHookServer");
-const server = new webHookServer_1.WebHookServer(Number(process.env.PORT));
-server.startServer();
-//# sourceMappingURL=index.js.map
