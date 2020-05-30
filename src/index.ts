b45de262d3d1857f9c30a4bb9cb10a4cf65f31f7
howzito <howzito@gmail.com>
2020-05-29 21:55:18 -0300

    Fixed dotenv. Added vscode settings.json with settings for automatic linting fix on save.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import dotenv from "dotenv"
-dotenv.config()
+import { config } from "dotenv"
+config()
 const port = Number(process.env.PORT) || 11000
 import { WebHookServer } from "./webHookServer"
 const server = new WebHookServer(port)
