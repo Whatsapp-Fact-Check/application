e4d98ff345c495e33c9e6391e940d073b8d44e19
sdasd <asdsd>
2020-06-13 13:43:23 -0300

    ErrorFormater

---
+++ src/messageFormater/errorFormater.spec.ts
@@ -0,0 +1,14 @@
+import { MessageResponseError } from "../messageResponse/messageResponseError"
+import { ErrorFormater } from "./errorFormater"
+
+test("should return error formatted string response", () => {
+  const type = "Error"
+
+  const messageResponse: MessageResponseError = {
+    type: type,
+    error: new Error("NotHitResultArray")
+  }
+  const instance = new ErrorFormater()
+  const expected = "Tivemos um erro do tipo: NotHitResultArray\nEntra em contato com a gente se continuar acontecendo!"
+  expect(instance.formatMessage(messageResponse)).toStrictEqual(expected)
+})
--- src/messageFormater/messageFormatter.spec.ts
+++ src/messageFormater/messageFormatter.spec.ts
@@ -1,6 +1,7 @@
 import { MessageResponse } from "@/messageResponse/messageResponse"
 import { MessageFormater } from "."
 import { MessageResponseHit, HitResult } from "../messageResponse/messageResponseHit"
+import { MessageResponseError } from "../messageResponse/messageResponseError"
 import { expectedTestHits } from "../__const__/consts"
 
 test("should return no hit formatted string response", () => {
@@ -23,5 +24,17 @@ test("should return hit formatted string response", () => {
   const instance = new MessageFormater()
   const expected =
     "Encontrei 1 registro(s) sobre esse tema! A seguir separei os mais relevantes: \nFato verificado: É Falso que Cuba desenvolveu vacina contra o coronavírus. Medicamentos cubanos têm sido utilizados para o tratamento de COVID-19 na China, mas não é uma vacina e a substância foi descoberta em outros lugares.\nVerificado por Agência Lupa\nLink: https://piaui.folha.uol.com.br/lupa/2020/03/13/verificamos-cuba-vacina-novo-coronavirus/ "
+  expect(instance.formatMessage(messageResponse)).toStrictEqual(expected)
+})
+
+test("should return error formatted string response", () => {
+  const type = "Error"
+
+  const messageResponse: MessageResponseError = {
+    type: type,
+    error: new Error("NotHitResultArray")
+  }
+  const instance = new MessageFormater()
+  const expected = "Tivemos um erro do tipo: NotHitResultArray\nEntra em contato com a gente se continuar acontecendo!"
   expect(instance.formatMessage(messageResponse)).toStrictEqual(expected)
 })
