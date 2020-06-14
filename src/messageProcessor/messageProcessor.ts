59b47630a9d55fb16aa7a53877d5e936db86c614
bunoviske <brunofcarvalho1996@gmail.com>
2020-06-14 20:43:58 +0200

    move messageProcessor from messageRouter to its own class

--- src/messageProcessor/messageProcessor.ts
+++ src/messageProcessor/messageProcessor.ts
@@ -1,7 +1,42 @@
 import { MessageRequest } from "@/messageRequest/messageRequest"
 import { MessageResponse } from "@/messageResponse/messageResponse"
+import { Constructor } from "@/messageRouter/messageRouter"
 
-export interface MessageProcessor {
+export interface MessageProcessorInterface {
   type: string
   processMessage: (message: MessageRequest) => Promise<MessageResponse>
+}
+
+const messageProcessorImplementations: Constructor<MessageProcessorInterface>[] = []
+function GetMessageProcessorImplementations(): Constructor<MessageProcessorInterface>[] {
+  return messageProcessorImplementations
+}
+export function RegisterMessageProcessor<T extends Constructor<MessageProcessorInterface>>(ctor: T): T {
+  messageProcessorImplementations.push(ctor)
+  return ctor
+}
+
+export class MessageProcessor {
+  private messageProcessors: Record<string, MessageProcessorInterface> = {}
+
+  constructor() {
+    this.initProcessor()
+  }
+
+  private initProcessor() {
+    const messageProcessorImplementations = GetMessageProcessorImplementations()
+    console.log(messageProcessorImplementations)
+    messageProcessorImplementations.forEach((MessageProcessor) => {
+      const instance = new MessageProcessor()
+      this.messageProcessors[instance.type] = instance
+    })
+  }
+
+  public async processMessage(message: MessageRequest): Promise<MessageResponse>{
+    const messageProcessor = this.messageProcessors[message.type]
+    if (messageProcessor) {
+      return await messageProcessor.processMessage(message)
+    }
+    throw Error("Message processor not registered: " + message.type)
+  }
 }
--- src/messageRouter/messageRouter.ts
+++ src/messageRouter/messageRouter.ts
@@ -1,9 +1,5 @@
-/**
- * Recebe a mensagem do whatsapp, escolhe um processador de mensagem, chama um messageFormater
- * Lembrar de importar algo do messageProcessor
- */
 
-import { MessageProcessor } from "../messageProcessor/messageProcessor"
+import { MessageProcessor } from "../messageProcessor"
 import { MessageRequest } from "../messageRequest/messageRequest"
 import { MessageResponse } from "../messageResponse/messageResponse"
 import { MessageFormater } from "../messageFormater"
@@ -13,32 +9,17 @@ export type Constructor<T> = {
   readonly prototype: T
 }
 
-const messageProcessorImplementations: Constructor<MessageProcessor>[] = []
-function GetMessageProcessorImplementations(): Constructor<MessageProcessor>[] {
-  return messageProcessorImplementations
-}
-export function RegisterMessageProcessor<T extends Constructor<MessageProcessor>>(ctor: T): T {
-  messageProcessorImplementations.push(ctor)
-  return ctor
-}
-
 export class MessageRouter {
-  private messageProcessors: Record<string, MessageProcessor> = {}
+  private messageProcessor: MessageProcessor
   private messageFormatter: MessageFormater
 
   constructor() {
+    this.messageProcessor = new MessageProcessor()
     this.messageFormatter = new MessageFormater()
-
-    const messageProcessorImplementations = GetMessageProcessorImplementations()
-    messageProcessorImplementations.forEach((MessageProcessor) => {
-      const instance = new MessageProcessor()
-      this.messageProcessors[instance.type] = instance
-    })
   }
 
   public async processMessage(message: MessageRequest): Promise<string> {
-    const messageProcessor = this.messageProcessors[message.type]
-    const result = await messageProcessor.processMessage(message)
+    const result = await this.messageProcessor.processMessage(message)
     return this.format(result)
   }
 
