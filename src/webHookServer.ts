724acf5c447b7d7c0df944246f9479b5fa524807
bunoviske <brunofcarvalho1996@gmail.com>
2020-06-14 17:14:21 +0200

    update webhook server to communicate with messageParser

--- src/webHookServer.ts
+++ src/webHookServer.ts
@@ -1,10 +1,4 @@
-/**
- *  * É o express server que recebe o request, monta um objeto MessageRequest e manda pro MessageRouter
- *
- *  */
-
-import { MessageParser } from "./messageRequest/messageParser"
-import { WppMessageParser } from "./messageRequest/wppMessageParser"
+import { MessageParser } from "./messageParser"
 import { MessageRouter } from "./messageRouter/messageRouter"
 import express from "express"
 import bodyParser from "body-parser"
@@ -13,10 +7,9 @@ export class WebHookServer {
   private port: number
   private app: any = express()
   private messageRouter: MessageRouter = new MessageRouter()
-  private messageParsers: Record<string, MessageParser> = {}
+  private messageParser: MessageParser = new MessageParser()
 
   constructor(port: number) {
-    this.messageParsers["wpp"] = new WppMessageParser() //TODO later -change to annotation and automatic implementation (same as messageRouter)
     this.port = port
     this.setupBodyParser()
     this.setupRoutes()
@@ -41,11 +34,17 @@ export class WebHookServer {
     })
 
     this.app.post("/wppMessage", async (req: any, res: any) => {
-      console.log("Received wpp msg request")
-      // console.log(req.body);
+      let parserType: string = "wpp"
+      let userResponse: string
 
-      const body = req.body.Body
-      const userResponse = await this.messageRouter.processMessage(this.messageParsers["wpp"].parse(body))
+      console.log("Received wpp msg request")
+
+      try {
+        userResponse = await this.messageRouter.processMessage(this.messageParser.parse(parserType, req.body))
+      } catch (error) {
+        userResponse = "Desculpe, houve um problema"
+        console.error(error)
+      }
 
       res.writeHead(200, { "Content-Type": "text/xml" })
       res.end(userResponse)
