ac13dd6cf09e59016d0ca0761e264a2382f332d8
bunoviske <brunofcarvalho1996@gmail.com>
2020-06-21 21:28:19 +0200

    truncate hit message with has more than 1600 chars

--- src/messageFormater/twilioFormater.ts
+++ src/messageFormater/twilioFormater.ts
@@ -1,17 +1,30 @@
 const MessagingResponse = require("twilio").twiml.MessagingResponse
 
 export class TwilioFormater {
-
   constructor() {}
+  private doubleLine = "\n\n"
 
   format(message: string): string {
-    if (message.length > 1600){
-        throw Error("Message has more than 1600 chars. Twilio support this number as maximum length")
+    if (message.length >= 1600) {
+      message = this.getTruncatedMessage(message)
     }
-    // message.slice(0, Math.min(message.length, 1600)) //truncate to max 1600 chars
-    
-    let twiml = new MessagingResponse();
+
+    let twiml = new MessagingResponse()
     twiml.message(message)
     return twiml.toString()
+  }
+
+  private getTruncatedMessage(message: string) : string {
+    message = message.slice(0, Math.min(message.length, 1600)) //truncate to max 1600 chars
+
+    let lastNewsIndex = message.lastIndexOf(this.doubleLine)
+    if (lastNewsIndex != -1){
+      message = message.slice(0, lastNewsIndex) //truncate to get the last news possible
+    }
+    else{
+      console.log("Twilio Formater - Did not find double line to split news. Return truncated message: " + message)
+    }
+
+    return message
   }
 }
