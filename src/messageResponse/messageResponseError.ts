2d2d82638d19826827a3ff4c1c9fe2e1b80d6247
bunoviske <brunofcarvalho1996@gmail.com>
2020-06-21 20:09:56 +0200

    add errorType

--- src/messageResponse/messageResponseError.ts
+++ src/messageResponse/messageResponseError.ts
@@ -1,5 +1,7 @@
 import { MessageResponse } from "./messageResponse"
 
+export type errorType = "invalidMedia" | "internal"
 export interface MessageResponseError extends MessageResponse {
   error: Error
+  errorType: errorType
 }
