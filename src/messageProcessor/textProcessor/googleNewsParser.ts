46348cc32c4be51c9ca7bcaa6f9388d323bab4a3
sdasd <asdsd>
2020-07-08 12:34:36 -0300

    Modularizar funcao parseMessage

--- src/messageProcessor/textProcessor/googleNewsParser.ts
+++ src/messageProcessor/textProcessor/googleNewsParser.ts
@@ -14,17 +14,17 @@ export class GoogleNewsParser extends HttpParser {
       const messageResponseError = this.createMessageResponseErrorInternal(response.error)
       return messageResponseError
     }
-
-    return await this.getMessageResponse(response)
+    const messageResponse = await this.getMessageResponse(response)
+    return messageResponse
   }
 
   private async getMessageResponse(response: string): Promise<MessageResponse> {
-    let messageResponseNoHit: MessageResponseNoHit = {
+    const messageResponseNoHit: MessageResponseNoHit = {
       type: "NoHit"
     }
     try {
       const news = await this.parseXml(response)
-      if (news.length === 0) {
+      if (news.length !== 0) {
         messageResponseNoHit.relatedNews = news
       }
       return messageResponseNoHit
@@ -36,41 +36,47 @@ export class GoogleNewsParser extends HttpParser {
 
   private parseXml(response: string): Promise<News[]> {
     return new Promise((resolve, reject) => {
-      let parsedJson = JSON.parse(response) //parse json because httpRequest stringify the http body as json
+      const parsedJson = JSON.parse(response) //parse json because httpRequest stringify the http body as json
       parseString(parsedJson, (err: any, result: any) => {
         if (err) {
           reject("Google News Xml Parser: " + err)
         }
 
-        let news: News[] = new Array<News>()
+        const parsedItems = result.rss.channel[0].item
+        const news: News[] = this.parseNews(parsedItems)
 
-        let parsedItems = result.rss.channel[0].item
-        let numNews = 3
-
-        for (let index = 0; index < numNews; index++) {
-          const element = parsedItems[index]
-          let title = element.title[0]
-          let link = element.link[0]
-          let date = element.pubDate[0]
-          let source = element.source[0]._
-          title = this.removeSourceFromTitle(title, source)
-          date = this.getFormatedDate(date)
-
-          news.push({
-            Title: title,
-            Link: link,
-            Date: date,
-            Source: source
-          })
-        }
         console.log(news)
         resolve(news)
       })
     })
   }
 
+  private parseNews(parsedItems: any): Array<News> {
+    const news: News[] = new Array<News>()
+
+    const numNews = 3
+
+    for (let index = 0; index < numNews; index++) {
+      const element = parsedItems[index]
+      let title = element.title[0]
+      const link = element.link[0]
+      let date = element.pubDate[0]
+      const source = element.source[0]._
+      title = this.removeSourceFromTitle(title, source)
+      date = this.getFormatedDate(date)
+
+      news.push({
+        Title: title,
+        Link: link,
+        Date: date,
+        Source: source
+      })
+    }
+    return news
+  }
+
   private removeSourceFromTitle(title: string, source: string) {
-    let truncateIndex = title.lastIndexOf(source) - 3 //subtract 3 to delete the '-' char
+    const truncateIndex = title.lastIndexOf(source) - 3 //subtract 3 to delete the '-' char
     if (truncateIndex >= 0) {
       return title.substring(0, truncateIndex)
     }
@@ -78,7 +84,29 @@ export class GoogleNewsParser extends HttpParser {
   }
 
   private getFormatedDate(date: string) {
-    let words = date.split(" ")
-    return words[1] + " " + words[2] + " " + words[3]
+    const words = date.split(" ")
+    return words[1] + " " + this.getPortugueseDate(words[2]) + " " + words[3]
+  }
+
+  private getPortugueseDate(date: string): string {
+    const dateTranslationMap = this.setDateTranslationMap()
+    return dateTranslationMap.get(date) as string
+  }
+
+  private setDateTranslationMap(): Map<string, string> {
+    const dateTranslationMap = new Map()
+    dateTranslationMap.set("Jan", "Janeiro")
+    dateTranslationMap.set("Feb", "Fevereiro")
+    dateTranslationMap.set("Mar", "Março")
+    dateTranslationMap.set("Apr", "Abril")
+    dateTranslationMap.set("May", "Maio")
+    dateTranslationMap.set("Jun", "Junho")
+    dateTranslationMap.set("Jul", "Julho")
+    dateTranslationMap.set("Aug", "Agosto")
+    dateTranslationMap.set("Sep", "Setembro")
+    dateTranslationMap.set("Oct", "Outubro")
+    dateTranslationMap.set("Nov", "Novembro")
+    dateTranslationMap.set("Dec", "Dezembro")
+    return dateTranslationMap
   }
 }
