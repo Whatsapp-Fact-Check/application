9bb051b7f294f858fb3b70a73b339c867180c5ef
bunoviske <brunofcarvalho1996@gmail.com>
2020-07-11 15:54:32 +0200

    add test with related news array

--- src/messageFormater/noHitFormater.spec.ts
+++ src/messageFormater/noHitFormater.spec.ts
@@ -1,7 +1,10 @@
 import { NoHitFormater } from "./noHitFormater"
-import { MessageResponseNoHit } from "../messageResponse/messageResponseNoHIt"
+import { MessageResponseNoHit, News } from "../messageResponse/messageResponseNoHIt"
 
-test("should return no hit formatted string response", () => {
+const bold = "*"
+const newLine = "\n"
+
+test("should return no hit and no related news message", () => {
   const type = "NoHit"
   const instance = new NoHitFormater()
   const expected = "Não encontramos nada correspondente 😓\n\n" +
@@ -11,3 +14,37 @@ test("should return no hit formatted string response", () => {
   }
   expect(instance.formatMessage(messageResponse)).toStrictEqual(expected)
 })
+
+
+test("should return related news response", () => {
+  const type = "NoHit"
+  const instance = new NoHitFormater()
+  const news: News[] =  [
+    {
+      Title: "OI",
+      Date: "22 Jun 2018",
+      Source: "Fonte",
+      Link: "http://oi.com"
+    }
+  ]
+
+  const expected = "Infelizmente não conseguimos encontrar nenhuma checagem de" +
+  " fakeNews sobre o tema, mas buscamos algumas notícias na internet relacionadas ao assunto que você pesquisou 👇\n\n" +
+  formatNews(news[0])
+
+  const messageResponse: MessageResponseNoHit = {
+    type: type,
+    relatedNews: news
+  }
+  expect(instance.formatMessage(messageResponse)).toStrictEqual(expected)
+})
+
+function formatNews(news: News): string {
+  const formattedString =
+    `Notícia: ${bold + news.Title}${bold + newLine}` +
+    `Fonte: ${bold + news.Source}${bold + newLine}` +
+    `Data da notícia: ${bold+ news.Date}${bold + newLine}` +
+    `Link: ${news.Link}`
+
+  return formattedString
+}
