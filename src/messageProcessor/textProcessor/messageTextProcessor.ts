78992a97e6f62b98a7cbed90c16813d71118a3ce
sdasd <asdsd>
2020-07-16 08:22:55 -0300

    change from fakeNewsDatabase to googleFactCheck

--- src/messageProcessor/textProcessor/messageTextProcessor.ts
+++ src/messageProcessor/textProcessor/messageTextProcessor.ts
@@ -21,6 +21,11 @@ export class MessageTextProcessor implements MessageProcessorInterface {
   private databaseUrl = "http://34.95.251.10:8080/checagem"
   private googleNewsUrl = "https://news.google.com/rss/search?q="
   private googleNewsLanguage = "&hl=pt-BR"
+  private googleFactCheckLanguageCode = "pt-BR"
+  private googleFactCheckUrl =
+    "https://factchecktools.googleapis.com/v1alpha1/claims:search?languageCode=" +
+    this.googleFactCheckLanguageCode +
+    "&query="
 
   constructor() {
     this.type = "text"
@@ -33,13 +38,21 @@ export class MessageTextProcessor implements MessageProcessorInterface {
     return new Promise<MessageResponse>(async (resolve, reject) => {
       const messageRequestText = this.toMessageRequestText(message)
 
-      let messageResponse = await this.searchFakeNewsDatabase(messageRequestText)
+      let messageResponse = await this.searchGoogleFactCheck(messageRequestText)
       if (this.isMessageResponseNoHit(messageResponse)) {
         messageResponse = await this.searchGoogleNews(messageRequestText)
       }
 
       resolve(messageResponse)
     })
+  }
+
+  private async searchGoogleFactCheck(messageRequestText: MessageRequestText): Promise<MessageResponse> {
+    const url: string = this.googleFactCheckUrl + messageRequestText.text + "&key=" + process.env.GOOGLE_API_KEY
+
+    const httpResponse = await this.httpRequestClient.get(url)
+    console.log("GOOGLE RESPONSE0" + httpResponse)
+    return this.pythonResponseParser.parseMessage(httpResponse) //mudar para googleFactCheckParser
   }
 
   private async searchFakeNewsDatabase(messageRequestText: MessageRequestText): Promise<MessageResponse> {
