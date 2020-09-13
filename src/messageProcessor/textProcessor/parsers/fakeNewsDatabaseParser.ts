8c20aeb90ef8ff4f595a0f08e993a6609ef4210d
Bruno Fernandes Carvalho <brunofcarvalho1996@gmail.com>
2020-09-13 22:34:11 +0200

    change parser because database response is different

--- src/messageProcessor/textProcessor/parsers/fakeNewsDatabaseParser.ts
+++ src/messageProcessor/textProcessor/parsers/fakeNewsDatabaseParser.ts
@@ -16,12 +16,68 @@ export class FakeNewsDatabaseParser extends HttpParser{
       return messageResponseError
     }
 
-    if (this.isArrayHitResult(response)) {
+    if (this.isFactCheckResult(response)) {
       return this.getMessageResponse(response)
     } else {
       const messageResponseError = this.createMessageResponseErrorInternal("UnknownTypePythonResponse")
       return messageResponseError
     }
+  }
+
+  private getMessageResponse(response: string): MessageResponse {
+
+    let hits = this.parseFactChecks(response)
+
+    if (hits.length === 0) {
+      //significa que n houve hit
+      const messageResponseNoHit: MessageResponseNoHit = {
+        type: "NoHit"
+      }
+      return messageResponseNoHit 
+    } 
+    else {
+      //significa q houve hits
+      hits = hits.slice(0, Math.min(hits.length, 3))
+      const messageResponseHit: MessageResponseHit = {
+        type: "Hit",
+        hits: hits
+      }
+      return messageResponseHit
+    }
+  }
+
+  private parseFactChecks(response: string) : HitResult[]{
+    let parsed = JSON.parse(response)
+    let hits: HitResult[] = new Array<HitResult>()
+
+    for (let i = 0; i < parsed.length; ++i) {
+      const elem = parsed[i]
+      hits.push({
+        Checado_por: elem.Agencia,
+        Checado: elem.Titulo,
+        Data: elem.Data,
+        Link: elem.Link
+      })
+    }
+
+    return hits
+  }
+
+  private isFactCheckResult(response: string): boolean {
+    const parsed = JSON.parse(response)
+    if (parsed instanceof Array) {
+      for (let i = 0; i < parsed.length; ++i) {
+        if (!this.isFactCheck(parsed[i])) {
+          return false
+        }
+      }
+      return true
+    }
+    return false
+  }
+
+  private isFactCheck(response: any): boolean {
+    return ("Agencia" in response && "Data" in response && "Link" in response && "Titulo" in response) 
   }
 
   private isJson(response: string): boolean {
@@ -33,44 +89,4 @@ export class FakeNewsDatabaseParser extends HttpParser{
     }
   }
 
-  private getMessageResponse(response: string): MessageResponse {
-    let hits = JSON.parse(response)
-    if (hits.length === 0) {
-      //significa que n houve hit
-      const messageResponseNoHit: MessageResponseNoHit = {
-        type: "NoHit"
-      }
-
-      return messageResponseNoHit as MessageResponse
-    } else {
-      //significa q houve hits
-      hits = hits.slice(0, Math.min(hits.length, 3))
-      const messageResponseHit: MessageResponseHit = {
-        type: "Hit",
-        hits: hits
-      }
-      return messageResponseHit as MessageResponse
-    }
-  }
-
-  private isArrayHitResult(response: string): boolean {
-    const parsed = JSON.parse(response)
-    if (parsed instanceof Array) {
-      for (let i = 0; i < parsed.length; ++i) {
-        if (!this.isHitResult(parsed[i])) {
-          return false
-        }
-      }
-      return true
-    }
-    return false
-  }
-
-  private isHitResult(hitResult: any): hitResult is HitResult {
-    if ("Checado" in hitResult && "Checado_por" in hitResult && "Data" in hitResult && "Link" in hitResult) {
-      return true
-    } else {
-      return false
-    }
-  }
 }
