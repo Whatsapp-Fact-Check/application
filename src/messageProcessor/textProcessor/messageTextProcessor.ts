25751ad4610596c7a90f2d713753bf4a51554109
Bruno Fernandes Carvalho <brunofcarvalho1996@gmail.com>
2020-10-03 21:54:25 +0200

    send hint message if required as active message

--- src/messageProcessor/textProcessor/messageTextProcessor.ts
+++ src/messageProcessor/textProcessor/messageTextProcessor.ts
@@ -2,15 +2,19 @@ import { MessageProcessorInterface, RegisterMessageProcessor } from "../messageP
 import { MessageRequest } from "../../messageRequest/messageRequest"
 import { MessageResponse } from "../../messageResponse/messageResponse"
 import { MessageRequestText } from "../../messageRequest/messageRequestText"
-import { FactCheckSearcher } from './searches/factCheck'
-import { RelatedNewsSearcher } from './searches/relatedNews'
+import { FactCheckSearcher } from "./searches/factCheck"
+import { RelatedNewsSearcher } from "./searches/relatedNews"
+import { HintProcessor } from "./hintProcessor"
+import { MessageResponseHint } from "@/messageResponse/messageResponseHint"
+import { ActiveMessageSender } from "../../asyncMessage/activeMessageSender"
 
 @RegisterMessageProcessor
 export class MessageTextProcessor implements MessageProcessorInterface {
   type: string
 
-  private factCheckSearcher  : FactCheckSearcher = new FactCheckSearcher()
-  private relatedNewsSearcher  : RelatedNewsSearcher = new RelatedNewsSearcher()
+  private factCheckSearcher: FactCheckSearcher = new FactCheckSearcher()
+  private relatedNewsSearcher: RelatedNewsSearcher = new RelatedNewsSearcher()
+  private hintProcessor: HintProcessor = new HintProcessor()
 
   constructor() {
     this.type = "text"
@@ -27,10 +31,20 @@ export class MessageTextProcessor implements MessageProcessorInterface {
           messageResponse = await this.relatedNewsSearcher.searchRelatedNews(text)
         }
         resolve(messageResponse)
+
+        let messageResponseHint = this.getMessageResponseHint(text)
+        if (messageResponseHint.hint != "NoHint") {
+          ActiveMessageSender.sendMessage(messageRequestText.id, "teste")
+        }
+
       } catch (error) {
         reject(error)
       }
     })
+  }
+
+  private getMessageResponseHint(text: string): MessageResponseHint {
+    return this.hintProcessor.processText(text)
   }
 
   private isMessageResponseNoHit(messageResponse: MessageResponse): boolean {
